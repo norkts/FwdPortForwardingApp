6541900e50e5135754af89cbbe7d53a7dc63da33
norkts <norkts@gmail.com>
2026-09-01 21:18:28 +0800

    feat: add floating window permission check in MainActivity

--- app/src/main/java/com/elixsr/portforwarder/ui/MainActivity.java
+++ app/src/main/java/com/elixsr/portforwarder/ui/MainActivity.java
@@ -52,15 +52,13 @@ import com.elixsr.portforwarder.ui.intro.MainIntro;
 import com.elixsr.portforwarder.ui.preferences.HelpActivity;
 import com.elixsr.portforwarder.ui.preferences.SettingsActivity;
 import com.elixsr.portforwarder.ui.rules.NewRuleActivity;
-import com.google.android.gms.analytics.Tracker;
-import com.google.firebase.analytics.FirebaseAnalytics;
+
 
 
 public class MainActivity extends BaseActivity {
 
     private static final String TAG = "MainActivity";
-    private static final String FORWARDING_MANAGER_KEY = "ForwardingManager";
-    private static final String FORWARDING_SERVICE_KEY = "ForwardingService";
+        private static final String FORWARDING_SERVICE_KEY = "ForwardingService";
 
     private List<RuleModel> ruleModels;
     private static RuleListAdapter ruleListAdapter;
@@ -77,9 +75,8 @@ public class MainActivity extends BaseActivity {
     private Intent forwardingServiceIntent;
     private RuleDao ruleDao;
     private PercentRelativeLayout mRuleListEmptyView;
-    private Tracker tracker;
 
-    private FirebaseAnalytics mFirebaseAnalytics;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -95,7 +92,7 @@ public class MainActivity extends BaseActivity {
         getSupportActionBar().setIcon(R.drawable.ic_nav_logo);
 
         // Obtain the FirebaseAnalytics instance.
-        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+        
 
         // Determine if this is first start - and whether to show app intro
         onFirstStart();
@@ -165,14 +162,14 @@ public class MainActivity extends BaseActivity {
                 mStatusIntentFilter);
 
         // Get tracker.
-        tracker = ((FwdApplication) this.getApplication()).getDefaultTracker();
 
         Log.i(TAG, "Finished onCreate");
     }
 
     @Override
     public void onResume() {
-        super.onResume();
+    
+    ForwardingManager.getInstance().init(this);        super.onResume();
         this.ruleModels.clear();
         this.ruleModels.addAll(ruleDao.getAllRuleModels());
         this.ruleListAdapter.notifyDataSetChanged();
@@ -279,13 +276,7 @@ public class MainActivity extends BaseActivity {
 
     }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-
-        outState.putSerializable(FORWARDING_MANAGER_KEY, this.forwardingManager);
-    }
-
+    
     @Override
     protected void onDestroy() {
         super.onDestroy();
